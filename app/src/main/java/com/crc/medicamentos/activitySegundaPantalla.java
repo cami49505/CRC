80f80614b87a45582fa3d11ef9367b167a195742
Carla <carla57mam@gmail.com>
2020-05-03 12:30:08 -0400

    interaccion registro de medicamentos, bienvenida

--- app/src/main/java/com/crc/medicamentos/activitySegundaPantalla.java
+++ app/src/main/java/com/crc/medicamentos/activitySegundaPantalla.java
@@ -1,7 +1,9 @@
 package com.crc.medicamentos;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -10,15 +12,19 @@ import androidx.appcompat.app.AppCompatActivity;
 public class activitySegundaPantalla extends AppCompatActivity {
 
     TextView Bienvenida;
+    Button Empecemos;
 
 
-
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.segunda_pantalla);
 
         Bienvenida = findViewById(R.id.TextWelcome);
+
+        Empecemos = findViewById(R.id.StartI);
+
         Intent intent = getIntent();
 
         if (intent.hasExtra("user")) {
@@ -30,5 +36,14 @@ public class activitySegundaPantalla extends AppCompatActivity {
 
 
 
+
+
+        Empecemos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activitySegundaPantalla.this,RegistroMedicamentosActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
