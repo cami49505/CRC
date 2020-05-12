b801eff2c3af0d784e9eb1a333627d7cf567b6c1
ros-blanco <62888466+ros-blanco@users.noreply.github.com>
2020-05-12 18:22:46 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/values/colors.xml

--- app/src/main/java/com/crc/medicamentos/RegistroMedicamentosActivity.java
+++ app/src/main/java/com/crc/medicamentos/RegistroMedicamentosActivity.java
@@ -4,8 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.Checkable;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -31,16 +29,13 @@ public class RegistroMedicamentosActivity extends AppCompatActivity {
 
 
 
-
-
-
         setContentView(R.layout.activity_registro_medicamentos);
         salir = findViewById(R.id.btSalir);
 
 
 
         salir.setOnClickListener(new View.OnClickListener() {
-            @Override
+           @Override
             public void onClick(View v) {
                 Intent intent= new Intent(RegistroMedicamentosActivity.this,MainActivity.class);
                 startActivity(intent);
